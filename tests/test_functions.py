4ae6086e8dadedcfbb0ce95d4d3a96abe66fb848
voukau <voukau@gmail.com>
2020-11-18 14:21:11 +0200

    Apply test naming workaround

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,26 +5,26 @@ import piexif
 
 class TestFunctions(unittest.TestCase):
 
-    def test_get(self):
+    def test_01_get(self):
         target = "../samples/Canon_40D.jpg"
         exif_dict = piexif.load(target)
         self.assertIsNotNone(exif_dict)
 
-    def test_remove(self):
+    def test_02_remove(self):
         target = "../samples/Canon_40D.jpg"
         piexif.remove(target)
         for i in ("0th", "Exif", "GPS", "Interop", "1st"):
             exif_dict = piexif.load(target)
             self.assertEqual(exif_dict[i], {})
 
-    def test_transfer(self):
+    def test_03_transfer(self):
         source = "../samples/Nikon_COOLPIX_P1.jpg"
         target = "../samples/Canon_40D.jpg"
         piexif.transplant(source, target)
         exif_dict = piexif.load(target)
         self.assertIsNotNone(exif_dict)
 
-    def test_remove_dir(self):
+    def test_04_remove_dir(self):
         target = "../samples"
         directory = os.fsencode(target)
         for file in os.listdir(directory):
