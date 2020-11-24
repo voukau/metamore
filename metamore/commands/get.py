9f74d37b538c3d9875e51538aca5f2d5b8087f89
voukau <voukau@gmail.com>
2020-11-24 18:30:33 +0200

    Change -get and --no-thumbnail logic

--- metamore/commands/get.py
+++ metamore/commands/get.py
@@ -11,11 +11,17 @@ def cli(target, out, nothumb):
     """Get EXIF metadata from file(s)"""
     exif_dict = piexif.load(target)
 
-    if nothumb:
-        del exif_dict["thumbnail"]
-
     if out:
+        if nothumb:
+            del exif_dict["thumbnail"]
         with open(os.path.splitext(target)[0]+'_meta.txt', 'w') as f:
             print(exif_dict, file=f)
     else:
-        print(exif_dict)
+        del exif_dict["thumbnail"]
+        for ifd_name in exif_dict:
+            print(f"\n{ifd_name}")
+            for key in exif_dict[ifd_name]:
+                try:
+                    print(f"\t {key}: {exif_dict[ifd_name][key][:10]}")
+                except:
+                    print(f"\t {key}: {exif_dict[ifd_name][key]}")
