8ca6f61e212e352caabf945b5ee0dc3d70c03363
voukau <voukau@gmail.com>
2020-11-24 21:21:52 +0200

    Release v0.1.3

--- metamore/commands/remove.py
+++ metamore/commands/remove.py
@@ -8,8 +8,8 @@ import os
 @click.option('-a', '--all', is_flag=True, help='Remove all EXIF data from a file.')
 @click.option('-d', '--dir', is_flag=True, help='Target is a directory. Remove EXIF data from all files in target '
                                                 'directory.')
-# @click.option('-k', '--key', type=click.Choice(['0th', 'Exif', 'GPS', 'Interop', '1st'],case_sensitive=False,
-# help='Remove EXIF data stored in particular key.')
+# @click.option('-k', '--key', type=click.Choice(['0th', 'Exif', 'GPS', 'Interop', '1st'], case_sensitive=False),
+#                                                 help='Remove EXIF data stored in particular key.')
 def cli(target, all, dir):
     """Remove EXIF metadata from file(s)"""
     if dir:
