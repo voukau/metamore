fd23eab599358e4a16f3bc446e92663b4cea7f82
voukau <voukau@gmail.com>
2020-11-26 16:10:42 +0200

    Get rid of remove --all flag

--- README.md
+++ README.md
@@ -9,18 +9,17 @@ To install Metamore simply run: `pip install metamore`. If `pip` recommends you 
 ### Options
 
 - get - Get EXIF metadata from file(s)
-- remove - Remove EXIF metadata from file(s)
+- remove - Remove all EXIF metadata from file(s)
 - transfer - Transfer EXIF metadata from one file to another
 
 ### Examples
 
 After installation and adding install directory to PATH Metamore is ready to go. Here is a couple of examples of the commands you can run:
 - `mm get -o Canon_40D.jpg` - Will save all the EXIF metadata found in the file 'Canon_40D.jpg' to a text file.
-- `mm remove -ad Pictures/` - Will remove all the EXIF metadata from all the .jpg/.jpeg files in the directory 'Pictures'.
+- `mm remove -d Pictures/` - Will remove all the EXIF metadata from all the .jpg/.jpeg files in the directory 'Pictures'.
 - `mm transfer Canon_40D.jpg Nikon_D70.jpg` - Will transfer EXIF metadata from the first file to the second one.
 
 ## ToDo
 
 - [x] Remove metadata from all files in the directory
 - [ ] Listen for new files in the directory and automatically remove their metadata
-- [ ] ~~Remove metadata from specific key (`mm remove --key`)~~ <- Piexif library only supports deletion of all EXIF keys from the file altogether apparently.
--- metamore/commands/remove.py
+++ metamore/commands/remove.py
@@ -5,26 +5,20 @@ import os
 
 @click.command()
 @click.argument('target', type=click.Path(exists=True))
-@click.option('-a', '--all', is_flag=True, help='Remove all EXIF data from a file.')
 @click.option('-d', '--dir', is_flag=True, help='Target is a directory. Remove EXIF data from all files in target '
                                                 'directory.')
-# @click.option('-k', '--key', type=click.Choice(['0th', 'Exif', 'GPS', 'Interop', '1st'], case_sensitive=False),
-#                                                 help='Remove EXIF data stored in particular key.')
-def cli(target, all, dir):
+def cli(target, dir):
     """Remove EXIF metadata from file(s)"""
     if dir:
         directory = os.fsencode(target)
-
-        if all:
-            no_jpg_found = True
-            for file in os.listdir(directory):
-                filename = os.fsdecode(file)
-                if filename.endswith(".jpg") or filename.endswith(".jpeg"):
-                    no_jpg_found = False
-                    piexif.remove(target + '/' + filename)
-            if no_jpg_found is True:
-                print("Error: No JPG/JPEG files found in the target directory")
+        no_jpg_found = True
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+                no_jpg_found = False
+                piexif.remove(target + '/' + filename)
+        if no_jpg_found is True:
+            print("Error: No JPG/JPEG files found in the target directory")
 
     else:
-        if all:
-            piexif.remove(target)
+        piexif.remove(target)
